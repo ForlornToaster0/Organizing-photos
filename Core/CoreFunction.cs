80ac3a49e1890dd70417caaf566905bf679b54b6
samuel andersson <samuel.ander@hotmail.com>
2022-03-18 19:10:39 +0100

    fixed a directory problem

--- Core/CoreFunction.cs
+++ Core/CoreFunction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,44 +9,46 @@ namespace Core
 {
     public class CoreFunction
     {
+
+
         public static void Task(string photoPath, string newPath)
         {
 
-            FindFolder findFolder = new();
             NewFolders newFolders = new();
-            var subdirectoryEntries = Subdirectories.GetSubDirectories(photoPath);
             int i = 0;
             List<DateTime> folders = new();
-            foreach (var subdirectoryEntry in subdirectoryEntries)
+
+            string[] allfiles = Directory.GetFiles(photoPath, "*.*", SearchOption.AllDirectories);
+            DirectoryInfo di = new(photoPath);
+
+            var extensions = new[] { ".GIF", ".JPG", ".PNG", ".CR2", ".ARW", ".ORF" };
+
+            foreach (var fi in di.GetFiles("*", SearchOption.AllDirectories))
             {
-                string fullPath = (subdirectoryEntry);
 
-                DirectoryInfo di = new(fullPath);
-
-                var extensions = new[] { "*.gif", "*.jpg", "*.png", "*.CR2", "*.ARW", "*.ORF" };
-                foreach (var extension in extensions)
+                string fileName = fi.Extension;
+                if (extensions.Contains(fileName.ToUpper()))
                 {
-                    foreach (var fi in di.GetFiles(extension, SearchOption.AllDirectories))
+
+                    string fullPath = $"{fi.FullName}";
+
+                    if (fullPath != null)
                     {
-                        fullPath = $"{fi.FullName}";
-                        string fileName = fi.Extension;
 
-                        if (fullPath != null)
+                        var test = FindFolder.OroginDate(photoPath);
+                        folders.Add(test);
+                        if (folders[0].Month != test.Month)
                         {
-
-                            var test = FindFolder.OroginDate(fullPath);
-                            folders.Add(test);
-                            if (folders[0].Month != test.Month)
-                            {
-                                folders.Clear();
-
-                            }
-                            newFolders.Creating(fullPath, newPath, test, fileName, i);
-
+                            folders.Clear();
 
                         }
+                        newFolders.Creating(photoPath, newPath, test, fileName, i);
+
+
                     }
+
                 }
+
 
             }
         }
